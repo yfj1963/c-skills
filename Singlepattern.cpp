0ba54d5e138d5f755c67110360c45b7ba182c043
yfj1963 <yfj_dut@163.com>
2017-06-16 13:47:40 +0800

    单例模式。
    1.单例模式中的构造函数必须为私有成员。
    2.类中必须有一个实例。
    3.必须有个静态成员的single *instance来指向自己；

--- Singlepattern.cpp
+++ Singlepattern.cpp
@@ -2,7 +2,22 @@ class single
 {
 private:
 single(){}
+~single(){}
 public:
-static single instance;
-
+   static single * getInstance()
+  {
+    if(instance == null)
+    {
+      instance = new single();
+    }
+    return *instance;
+  }
+static single *instance;
 };
+single *single::instance = null;
+int main()
+{
+single *single1 = single::getInstance();
+single *single2 = single::getInstance();
+delete single::getInstance();
+}
