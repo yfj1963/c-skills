af37034ad98ee6630a0b37d60387c3756ac0ba82
yfj1963 <yfj_dut@163.com>
2017-06-17 16:21:46 +0800

    适配器模式主要是采用以接口的方式来调用新增加的代码。

---
+++ adapterPattern.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class target
+{
+public:
+virtual void request()
+{
+cout<<"target request"<<endl;
+}
+virtual ~ target(){}
+};
+class adapter
+{
+public:
+void specificRequest()
+{
+cout<<"adapter secificRequest"<<endl;
+}
+};
+class adapterchild : public adapter private target
+{
+public:
+adapterchild(adater *adp){_adp = adp;}
+~adapterchild(){}
+void request()
+{
+_adp->specificRequest();
+}
+private:
+adapter *_adp;
+};
+int main()
+{
+adapter *adp = new adater();
+target *tg = new new adapterchild(adp);
+tg->request();
+cin.get();
+return 1;
+}
